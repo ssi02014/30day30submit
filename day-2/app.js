fa3963ac23c9dd5fef2eadb466a430cb1efc5f73
ssi02014 <64779472+ssi02014@users.noreply.github.com>
2020-12-12 18:52:00 +0900

    Theme Switcher

---
+++ day-2/app.js
@@ -0,0 +1,12 @@
+const switcher = document.querySelector('.switcher');
+
+switcher.addEventListener('click', () => {
+    switcher.classList.toggle('active');
+    if(switcher.classList.contains('active')){
+        document.body.setAttribute('data-theme', 'dark');
+    } else {
+        document.body.setAttribute('data-theme', 'light');
+    }
+
+    
+})
